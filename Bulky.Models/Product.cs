403bc9304a9b19e68f2b0aa531d2e4e2362930b6
Depeesh Gurung <depeshgurung44@gmail.com>
2024-05-27 14:13:26 +0530

    ImageUrl is added to Product table

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -37,4 +37,6 @@ public class Product
     public int CategoryId { get; set; }
     [ForeignKey("CategoryId")]
     public Category Category { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    public string ImageUrl { get; set; }
 }
