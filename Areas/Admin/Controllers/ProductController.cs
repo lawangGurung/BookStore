af88612e3e50478d57be2cd5881e98cc91532c94
Depeesh Gurung <depeshgurung44@gmail.com>
2024-06-13 14:50:23 +0530

    Datatable is added, Edit and Delete functionality is added

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -63,6 +63,7 @@ namespace MyApp.Namespace
         {
             if (ModelState.IsValid)
             {
+                // this is to save the image and here "file" is the image
                 string wwwRootPath = _webHostEnvironment.WebRootPath;
 
                 if (file != null)
@@ -70,6 +71,7 @@ namespace MyApp.Namespace
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images/products");
 
+                    //this is for updating the image
                     if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
                     {
                         // Delete the old image
@@ -105,11 +107,11 @@ namespace MyApp.Namespace
             else
             {
                 productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-                {
-                    Text = u.Name,
-                    Value = u.Id.ToString()
+                    {
+                        Text = u.Name,
+                        Value = u.Id.ToString()
 
-                });
+                    });
 
                 return View(productVM);
             }
@@ -117,45 +119,71 @@ namespace MyApp.Namespace
         }
 
 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
+        // public IActionResult Delete(int? id)
+        // {
+        //     if (id == null || id == 0)
+        //     {
+        //         return NotFound();
+        //     }
 
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+        //     Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
 
-            if(productFromDb == null)
-            {
-                return NotFound();
-            }
+        //     if(productFromDb == null)
+        //     {
+        //         return NotFound();
+        //     }
 
-            return View(productFromDb);
-        }
+        //     return View(productFromDb);
+        // }
 
-        [HttpPost]
-        public IActionResult Delete(Product obj)
-        {
-            if(obj == null)
-            {
-                return NotFound();
-            }
+        // [HttpPost]
+        // public IActionResult Delete(Product obj)
+        // {
+        //     if(obj == null)
+        //     {
+        //         return NotFound();
+        //     }
 
-            _unitOfWork.Product.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Product removed successfully";
-            return RedirectToAction("Index");
-        }
+        //     _unitOfWork.Product.Remove(obj);
+        //     _unitOfWork.Save();
+        //     TempData["success"] = "Product removed successfully";
+        //     return RedirectToAction("Index");
+        // }
 
         #region API CALLS
 
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
-            return Json(new {data = objProductList});
-        }
+            [HttpGet]
+            public IActionResult GetAll()
+            {
+                List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+                return Json(new {data = objProductList});
+            }
+
+
+            [HttpDelete]
+            public IActionResult Delete(int? id)
+            {
+                Product? productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+
+                if (productToBeDeleted == null)
+                {
+                    return Json(new {success = false, message = "Error While Deleting"});
+                }
+
+            // image is also needed to be deleted before deleting the obj
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImageUrl.TrimStart('/'));
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+
+                _unitOfWork.Product.Remove(productToBeDeleted);
+                _unitOfWork.Save();
+
+                return Json(new {success = true, message = "Delete Successful"});
+            }
 
         #endregion
 
