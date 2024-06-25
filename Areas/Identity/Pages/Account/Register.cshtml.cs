f76d7b0539780279d7416f505a670286720ffedd
Depeesh Gurung <depeshgurung44@gmail.com>
2024-06-25 15:31:23 +0530

    Company added to the Register

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -6,10 +6,12 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using Bulky.DataAccess;
 using Bulky.Models;
 using Bulky.Utility;
 using Microsoft.AspNetCore.Authentication;
@@ -34,6 +36,7 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -41,7 +44,8 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender,
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager,
+            IUnitOfWork unitOfWork)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -50,6 +54,7 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
             _logger = logger;
             _emailSender = emailSender;
             _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
         }
 
         /// <summary>
@@ -118,6 +123,9 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
             public string State { get; set; }
             public string PostalCode { get; set; }
             public string PhoneNumber { get; set; }
+            public int? CompanyId { get; set; }
+            [ValidateNever]
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
         }
 
 
@@ -135,6 +143,10 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
                 RoleList = _roleManager.Roles.Select(i=> new SelectListItem {
                     Text = i.Name,
                     Value = i.Name
+                }),
+                CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
                 })
             };
 
@@ -158,6 +170,11 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
                 user.State = Input.State;
                 user.PostalCode = Input.PostalCode;
                 user.PhoneNumber = Input.PhoneNumber; 
+
+                if(Input.Role == SD.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
                 
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
---
+++ Bulky.DataAccess/Migrations/20240625074830_AddCompanyToUsers.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanyToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Companies_CategoryId",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "AspNetUsers",
+                newName: "CompanyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AspNetUsers_CategoryId",
+                table: "AspNetUsers",
+                newName: "IX_AspNetUsers_CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "CompanyId",
+                table: "AspNetUsers",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AspNetUsers_CompanyId",
+                table: "AspNetUsers",
+                newName: "IX_AspNetUsers_CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Companies_CategoryId",
+                table: "AspNetUsers",
+                column: "CategoryId",
+                principalTable: "Companies",
+                principalColumn: "Id");
+        }
+    }
+}
