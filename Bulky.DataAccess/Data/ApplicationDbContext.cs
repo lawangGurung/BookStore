5e9259b060d13d126e95537e0c4cbaa8a4538f35
Depeesh Gurung <depeshgurung44@gmail.com>
2024-06-27 11:50:16 +0530

    ShoppingCart Added to the code and to databse

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
       public DbSet<Product> Products { get; set; }
       public DbSet<ApplicationUser> ApplicationUsers { get; set; }
       public DbSet<Company> Companies { get; set; }
+      public DbSet<ShoppingCart> ShoppingCarts { get; set; }
       protected override void OnModelCreating(ModelBuilder modelBuilder)
       {
         base.OnModelCreating(modelBuilder);
--- Bulky.DataAccess/Migrations/20240627061236_AddShoppingCartToDb.Designer.cs
+++ Bulky.DataAccess/Migrations/20240627061236_AddShoppingCartToDb.Designer.cs
@@ -4,16 +4,19 @@ using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Bulky.DataAccess
+namespace BulkyWeb.Bulky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240627061236_AddShoppingCartToDb")]
+    partial class AddShoppingCartToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
