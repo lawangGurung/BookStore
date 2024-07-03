61b333e7eaa1fd018368f8d129baafb93d8a3c94
Depeesh Gurung <depeshgurung44@gmail.com>
2024-07-03 14:28:34 +0530

    Order Management sort complete

--- Areas/Admin/Controllers/OrderController.cs
+++ Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 using Bulky.DataAccess;
 using Bulky.Models;
+using Bulky.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyApp.Namespace
@@ -20,9 +21,27 @@ namespace MyApp.Namespace
          #region API CALLS
 
             [HttpGet]
-            public IActionResult GetAll()
+            public IActionResult GetAll(string status)
             {
-                List<OrderHeader> objOrderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+                IEnumerable<OrderHeader> objOrderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+                switch(status) {
+                    case "pending" :
+                        objOrderList = objOrderList.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayement);
+                        break;
+                    case "inprocess" :
+                        objOrderList = objOrderList.Where(u => u.OrderStatus == SD.StatusInProcess);
+                        break;
+                    case "completed" :
+                        objOrderList = objOrderList.Where(u => u.OrderStatus == SD.StatusShipped);
+                        break;
+                    case "approved" :
+                        objOrderList = objOrderList.Where(u => u.OrderStatus == SD.StatusApproved);
+                        break;
+                    default:
+                        break;
+                }
+
                 return Json(new {data = objOrderList});
             }
 
